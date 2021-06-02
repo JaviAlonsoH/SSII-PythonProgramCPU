9b35bb0e9dc7ba62015082a9db8bce7ec324a5c6
Javi <javier.alonso@live.u-tad.com>
2021-06-02 02:41:44 +0200

    Algoritmo Fifo Creo que funciona
    He creado el algoritmo tipo FIFO y creo que trata bien los datos. En la tabla se muestran bien, ahora probaré a implementarlos en la grafica para que se pueda ver.

--- mainProgram.py
+++ mainProgram.py
@@ -17,8 +17,8 @@ class Win(tk.Tk):
         # WINDOW CONFIGURATION
 
         self.title('Parameters')
-        self.geometry('1000x400')
-        self.c = Canvas(self, height=400, width=1000, bg='#d1d1d1')
+        self.geometry('1000x500')
+        self.c = Canvas(self, height=600, width=1000, bg='#d1d1d1')
         self.c.pack()
 
         # WINDOW ELEMENTS
@@ -28,30 +28,116 @@ class Win(tk.Tk):
         self.lbl_proc_number = tk.Label(self.c, text='Numero de procesos: ', bg='#d1d1d1')
         self.inputBox_proc_number = tk.Entry(self.c)
 
-        self.btnSaveProc = tk.Button(self.c, text="Agregar", command=lambda: self.ComboBox())
+        self.btnSaveProc = tk.Button(self.c, text="Agregar", command=lambda: self.onSelect())
 
-
+        self.lbl_select_process = tk.Label(self.c, text='Selecciona el proceso: ', bg='#d1d1d1')
         self.comboBox_processes = ttk.Combobox(self.c, values=self.processes, width=18)
+        self.comboBox_processes.bind(self.onSelect)
 
-        self.lbl_arrive_time = tk.Label(self.c, text='Numero de procesos: ', bg='#d1d1d1')
+        self.lbl_arrive_time = tk.Label(self.c, text='Tiempo de llegada: ', bg='#d1d1d1')
         self.inputBox_arrive_time = tk.Entry(self.c)
-        self.lbl_exe_time = tk.Label(self.c, text='Numero de procesos: ', bg='#d1d1d1')
+
+        self.lbl_exe_time = tk.Label(self.c, text='Tiempo de ejecución: ', bg='#d1d1d1')
         self.inputBox_exe_time = tk.Entry(self.c)
 
-        self.btnGenSim = tk.Button(self.c, text="Generar Gráfica", width=25, command= lambda: self.ShowData())
+        self.btnSaveData= tk.Button(self.c, text="Guardar Proceso", width=20, command=self.SaveData)
 
-        self.c.create_window(100, 100, window=self.lbl_proc_number)
-        self.c.create_window(300, 100, window=self.inputBox_proc_number)
-        self.c.create_window(300, 150, window=self.comboBox_processes)
-        self.c.create_window(300, 200, window=self.inputBox_arrive_time)
-        self.c.create_window(300, 250, window=self.inputBox_exe_time)
-        self.c.create_window(450, 100, window=self.btnSaveProc)
-        self.c.create_window(300, 300, window=self.btnGenSim)
+        self.btnGenSim = tk.Button(self.c, text="Generar Gráfica", width=25, height=2, command=self.ShowData)
+
+        # ALGORITMOS
+
+        self.opt = IntVar()
+
+        self.fifo = tk.Radiobutton(self.c, text="FIFO", value=1, bg='#d1d1d1', variable=self.opt)
+        self.sjf = tk.Radiobutton(self.c, text="SJF", value=2, bg='#d1d1d1', variable=self.opt)
+        self.roundRobin = tk.Radiobutton(self.c, text="Round Robin", value=3, bg='#d1d1d1', variable=self.opt)
+
+        self.opt.set(None)
+
+
+        self.c.create_window(100, 70, window=self.lbl_proc_number)
+        self.c.create_window(300, 70, window=self.inputBox_proc_number)
+        self.c.create_window(450, 70, window=self.btnSaveProc)
+
+        self.c.create_window(100, 125, window=self.lbl_select_process)
+        self.c.create_window(300, 125, window=self.comboBox_processes)
+
+        self.c.create_window(100, 180, window=self.lbl_arrive_time)
+        self.c.create_window(300, 180, window=self.inputBox_arrive_time)
+
+        self.c.create_window(100, 230, window=self.lbl_exe_time)
+        self.c.create_window(300, 230, window=self.inputBox_exe_time)
+
+        self.c.create_window(700, 100, window=self.fifo)
+        self.c.create_window(700, 150, window=self.sjf)
+        self.c.create_window(700, 200, window=self.roundRobin)
+
+        self.c.create_window(300, 280, window=self.btnSaveData)
+
+        self.c.create_window(500, 450, window=self.btnGenSim)
 
         # VARIABLES
 
-    def DataTreatment(self):
-        x = 0
+        self.num_processes = 0
+        self.exe_time = 0
+        self.response_time = 0
+        self.wait_time = 0
+        self.arrive_time = 0
+        self.ini = 0
+        self.end = 0
+        self.P = 0
+        self.R = 0
+        self.Quantum = 0
+
+        # DATOS TABLA
+
+        self.data_table = {
+        }
+
+    def onSelect(self):
+        self.processes = []
+        self.num_processes = int(self.inputBox_proc_number.get())
+        cont = 1
+        while len(self.processes) < self.num_processes:
+            nom = 'Proceso ' + str(cont)
+            self.processes.append(nom)
+            cont += 1
+        self.inputBox_proc_number.clipboard_clear()
+        self.comboBox_processes['values'] = (self.processes)
+        print(self.processes)
+
+
+    def SaveData(self):
+        self.arrive_time = self.inputBox_arrive_time.get()
+        self.exe_time = (self.inputBox_exe_time.get())
+        self.inputBox_exe_time.delete(0, 'end')
+        self.inputBox_arrive_time.delete(0, 'end')
+
+        self.response_time = int(self.wait_time) + int(self.exe_time)
+        self.ini = int(self.response_time) - int(self.exe_time)
+        self.end = int(self.response_time)
+        self.wait_time = int(self.response_time) - int(self.exe_time)
+        self.P = int(self.response_time) / int(self.exe_time)
+        self.R = int(self.exe_time) / int(self.response_time)
+
+        res = not self.data_table
+
+        if res:
+            row = {'Llegada': self.arrive_time, 'T. Ejecución': self.exe_time, 'Inicio': self.ini,
+                   'Fin': self.end,
+                   'T. Respuesta': self.response_time, 'T. Espera': self.wait_time, 'Penalización': self.P}
+            self.data_table[self.comboBox_processes.get()] = row
+
+        else:
+            for i in self.data_table.keys():
+                    row = {'Llegada': self.arrive_time, 'T. Ejecución': self.exe_time,
+                                'Inicio': self.data_table[i]['Fin'], 'Fin': self.end,
+                                'T. Respuesta': int(self.data_table[i]['T']) + int(self.response_time),
+                                'T. Espera': self.wait_time, 'Penalización': self.P}
+                    self.data_table[i] = row
+
+        print(self.data_table)
+
 
         # DATA WINDOW
 
@@ -59,6 +145,7 @@ class Win(tk.Tk):
 
         root = tk.Tk()
         root.configure(background="#d1d1d1")
+        root.geometry('1200x1000')
         root.title('CPU Process Graphic Representation')
 
         canvasData = tk.Canvas(root,height=0, width=1100)
@@ -74,25 +161,19 @@ class Win(tk.Tk):
         a.hlines(y=[39, 40, 41], xmin=[0, 25, 50], xmax=[len(xs)], colors='purple', linestyles='--', lw=2,
                    label='Multiple Lines')
         a.legend(bbox_to_anchor=(0.5, 1.3), loc="upper center", borderaxespad=5)
+        a.grid()
         canvas = FigureCanvasTkAgg(plot, master=root)
         canvas.get_tk_widget().pack()
         canvas.draw()
 
         # TABLE
 
-        data = {
-            '1': {'Source': 'OB', 'Campaign': 'X7765LKBTYYU-TAB-US', 'Impr.': 9457123, 'CTR': '0.12%', 'Spent': 45.78},
-            '2': {'Source': 'OB', 'Campaign': 'X6159TNFYTHY-PHONE-US-A', 'Impr.': 5456953, 'CTR': '0.27%',
-                  'Spent': 12.52},
-            '3': {'Source': 'OB', 'Campaign': 'X7765LGKJHYRT-TAB-US', 'Impr.': 9457123, 'CTR': '0.12%', 'Spent': 45.78},
-            '4': {'Source': 'OB', 'Campaign': 'X6159FTJKYUKKX-PHONE-US-A', 'Impr.': 5456953, 'CTR': '0.27%',
-                  'Spent': 12.52}
-        }
-
         tbl_frame = tk.Frame(root, width=1000, height=550, bg="lightcoral")
         tbl_frame.pack(side=tk.BOTTOM)
         model = TableModel()
-        table = TableCanvas(tbl_frame, model=model, data=data, editable=False, width=800, height=300)
+        table = TableCanvas(tbl_frame, model=model, data=self.data_table, editable=False, width=1000, height=300)
+        x = self.data_table.keys()
+        print(x)
 
         table.show()
 
